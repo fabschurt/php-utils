408d11d0ebed76d47e84dd8085a10fc74d813479
Fabien Schurter <fabien@fabschurt.com>
2016-05-09 19:18:20 +0200

    Add license blocks in test files

--- tests/unit/ArrayUtils/AbstractSortableStringArray.spec.php
+++ tests/unit/ArrayUtils/AbstractSortableStringArray.spec.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the fabschurt/php-utils package.
+ *
+ * (c) 2016 Fabien Schurter <fabien@fabschurt.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use FabSchurt\PhpUtils\ArrayUtils\AbstractSortableStringArray;
 
 describe(AbstractSortableStringArray::class, function () {
