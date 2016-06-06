1f0f76cc443eac81e0e04ce2acf8a701b3bf4d87
Fabien Schurter <fabien@fabschurt.com>
2016-06-06 17:11:22 +0200

    Rename and refactor test file

---
+++ tests/unit/SortableArray/NaturallySortableStringArray.spec.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the fabschurt/php-utils package.
+ *
+ * (c) 2016 Fabien Schurter <fabien@fabschurt.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use FabSchurt\Php\Utils\SortableArray\NaturallySortableStringArray;
+
+describe(NaturallySortableStringArray::class, function () {
+    beforeEach(function () {
+        $this->subjectFactory = function (array $data = null): NaturallySortableStringArray {
+            return new NaturallySortableStringArray($data ?? $this->dataFactory());
+        };
+
+        $this->dataFactory = function (): array {
+            return [
+                'Thymus Serpillum Extrakt',
+                'Thymol',
+                'Thymus Vulgaris',
+                'Thymus Mastichina Herb Oil',
+            ];
+        };
+    });
+
+    describe('->sortByTermResemblance()', function () {
+        it('naturally orders its elements according to resemblance with the passed term', function () {
+            expect($this->subjectFactory()->sortByTermResemblance('THYMUS VULGARIS FLOWER WATER'))
+                ->to->equal([
+                    'Thymus Vulgaris',
+                    'Thymus Serpillum Extrakt',
+                    'Thymus Mastichina Herb Oil',
+                    'Thymol',
+                ])
+            ;
+        });
+    });
+
+    describe('->asArray()', function () {
+        it('returns the wrapped source array untouched', function () {
+            $data = $this->dataFactory();
+            expect($this->subjectFactory($data)->asArray())->to->equal($data);
+        });
+    });
+});
