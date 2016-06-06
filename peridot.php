254720a344fbe918d7a10458b5248fef19d76e2b
Fabien Schurter <fabien@fabschurt.com>
2016-06-06 17:11:39 +0200

    Refactor tests

--- peridot.php
+++ peridot.php
@@ -1,16 +1,19 @@
 <?php
 
-/**
- * @return object[]
+/*
+ * This file is part of the fabschurt/php-utils package.
+ *
+ * (c) 2016 Fabien Schurter <fabien@fabschurt.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
-function get_example_array(): array
-{
-    $array = [];
-    for ($i = 0; $i < 3; ++$i) {
-        $object = new stdClass();
-        $object->name = "Object{$i}";
-        $array[$i] = $object;
-    }
 
-    return $array;
-}
+use Evenement\EventEmitterInterface;
+use Peridot\Console\Environment;
+
+return function (EventEmitterInterface $emitter) {
+    $emitter->on('peridot.start', function (Environment $environment) {
+        $environment->getDefinition()->getArgument('path')->setDefault('tests');
+    });
+};
